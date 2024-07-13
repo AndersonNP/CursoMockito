aa7e0eaa5f1a52e5ea119f8a518ac234d57a69c9
Anderson Nascimento <anderson.nascimento.info@gmail.com>
2024-07-13 17:15:35 -0300

    Aprendendo a utilização do SPY

---
+++ mockito_exemplos/src/main/java/com/nascimento/dio/Mensagem.java
@@ -0,0 +1,20 @@
+package com.nascimento.dio;
+
+public class Mensagem {
+
+    private String mensagem;
+
+    public Mensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    
+}
